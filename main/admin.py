bb3f48c7649673e8cb57ccef54c3a1ca14e60a7b
Oleh Synyshyn <oleg.synyshyn10@gmail.com>
2026-02-05 18:26:23 +0200

    last ever

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from .models import Category, Dish, Cart, CartItem, Order, OrderItem, Review
 
-from main.models import Category, Dish, Order, OrderItem
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "order")
+    list_editable = ("order",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
 
-admin.site.register(Category)
-admin.site.register(Dish)
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "is_available", "is_popular", "is_new")
+    list_filter = ("category", "is_available", "is_popular", "is_new")
+    search_fields = ("name", "ingredients")
+    list_editable = ("price", "is_available", "is_popular", "is_new")
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ("category", "name")
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Dish, DishAdmin)
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-
+admin.site.register(Review)
